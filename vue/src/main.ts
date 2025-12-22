2f744a47607450ab5751de17f264bba44b837d98
Henri mint <henrir1020@gmail.com>
2025-12-22 19:11:23 -0300

    feat: changed for local props header for router driven UI

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -4,7 +4,7 @@ import router from './router.ts'
 import './index.css'
 import cursorDirective from './utils/pointerDirective.ts'
 
-// async autentication before mounting the app for security reasons
+// async authentication before mounting the app for security reasons
 
 const app = createApp(App)
 app.use(router)
