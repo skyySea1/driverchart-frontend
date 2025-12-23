62b25be7bfb60be3016177ac318dd36b395e6185
Henri mint <henrir1020@gmail.com>
2025-12-23 20:38:21 -0300

    feat: add Dockerfile for multi-stage build and create .dockerignore

--- vue/src/stores/ModalStore.ts
+++ vue/src/stores/ModalStore.ts
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia';
 export const useModalStore = defineStore('modal', {
   state: () => ({
     activeModal: null as 'vehicle' | 'driver' | null,
-    data: null as unknown | null // Para passar um objeto se for edição
+    data: null as unknown | null 
   }),
   actions: {
     openModal(type: 'vehicle' | 'driver', payload: unknown = null) {
