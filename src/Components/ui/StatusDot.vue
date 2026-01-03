47a622e12939c7d8d5ad5bdfdc5b753e2c7a3d05
Skyysea1-gtk <henrir1020@gmail.com>
2026-01-02 23:33:36 -0300

    feat: add StatusDot component for status indication

---
+++ src/Components/ui/StatusDot.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  ok?: boolean
+  neutral?: boolean
+}>()
+</script>
+
+<template>
+  <div
+    class="w-3 h-3 rounded-full mx-auto"
+    :class="
+      neutral
+        ? ok ? 'bg-green-500' : 'bg-gray-300'
+        : ok ? 'bg-green-500' : 'bg-red-500'
+    "
+  ></div>
+</template>
