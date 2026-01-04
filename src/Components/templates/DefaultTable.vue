b79b1ddf3dbf52340b82752c8d6153ad4a557981
Skyysea1-gk <henrir1020@gmail.com>
2026-01-04 02:52:33 -0300

    Refactor: Standardize DefaultTable class attribute formatting

--- src/Components/templates/DefaultTable.vue
+++ src/Components/templates/DefaultTable.vue
@@ -8,58 +8,28 @@
             <th
               v-for="col in props.columns"
               :key="col.key"
-              :class="[
-                'p-4 whitespace-nowrap',
-                col.align === 'center'
-                  ? 'text-center'
-                  : col.align === 'right'
-                    ? 'text-right'
-                    : col.align === 'left'
-                      ? 'text-left'
-                      : '',
-              ]"
-            >
+              :class="[ 'p-4 whitespace-nowrap', col.align === 'center' ? 'text-center' : col.align === 'right'? 'text-right': col.align === 'left'? 'text-left' : '', ]">
               {{ col.label }}
             </th>
           </tr>
         </thead>
         <tbody class="divide-y divide-slate-100">
+
           <!-- Skeleton State -->
           <template v-if="props.loading">
             <tr v-for="i in 5" :key="i" >
               <td v-cursor v-for="col in props.columns" :key="col.key" class="p-4">
                 <div
-                  :class="[
-                    'h-4 skeleton rounded',
-                    col.align === 'center'
-                      ? 'mx-auto w-12'
-                      : col.align === 'right'
-                        ? 'ml-auto w-16'
-                        : 'w-24',
-                  ]"
-                ></div>
+                  :class="['h-4 skeleton rounded', col.align === 'center' ? 'mx-auto w-12' : col.align === 'right' ? 'ml-auto w-16' : 'w-24', ]">
+                </div>
               </td>
             </tr>
           </template>
 
           <template v-else>
-            <tr
-              v-for="(item, index) in props.items"
-              :key="item.id || index"
-              class="hover:bg-slate-50 transition-colors"
-            >
-              <td
-                v-for="col in props.columns"
-                :key="col.key"
-                :class="[
-                  'p-4 whitespace-nowrap',
-                  col.align === 'center'
-                    ? 'text-center'
-                    : col.align === 'right'
-                      ? 'text-right'
-                      : '',
-                ]"
-              >
+            <tr v-for="(item, index) in props.items" :key="item.id || index" class="hover:bg-slate-50 transition-colors">
+              <td v-for="col in props.columns" :key="col.key" :class="[ 'p-4 whitespace-nowrap',
+                   col.align === 'center' ? 'text-center' : col.align === 'right' ? 'text-right' : '', ]">
                 <slot :name="`cell(${col.key})`" :item="item" :value="item[col.key]">
                   <span class="text-slate-600">{{ item[col.key] || '-' }}</span>
                 </slot>
