02d1a37ec690bb148c0c7ffe4ce7487f40003fc0
Skyysea1-gk <henrir1020@gmail.com>
2026-01-04 02:52:34 -0300

    Feature: Introduce reusable BaseLoading component

---
+++ src/Components/ui/BaseLoading.vue
@@ -0,0 +1,13 @@
+<template>
+    <div  class="flex items-center justify-center h-64">
+      <div class="animate-spin rounded-full h-8 w-8 border-b-2 border-primary-600"></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
